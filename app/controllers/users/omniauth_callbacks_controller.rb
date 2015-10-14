e6a2edc86d601385f9e52ea363fc4aa257d1341c
zsid <zhivko.siderov@gmail.com>
2015-10-14 11:29:32 +0100

    Facebook Working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    p request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
